78b7b69ec690a06bd351c5297037bd628a27ad91
Bueno Martins <gbueno@indracompany.com>
2019-05-15 14:42:42 -0300

    Alteração nos arquivos automatizados do proxy para git

--- proxy-para-gitbash/ativar_git_proxy_auto.ps1
+++ proxy-para-gitbash/ativar_git_proxy_auto.ps1
@@ -1,8 +1,13 @@
-$usergit = Read-Host -Prompt "Digite seu nome de usuário git"
 
-git config --global http.proxy http://$usergit@proxylatam.indra.es:8080
-git config --global https.proxy https://$usergit@proxylatam.indra.es:8080
+#Captura o usuário de rede digitado:
+$userRede = Read-Host -Prompt "Digite seu nome de usuário git"
 
+#Configurando por comandos git:
+git config --global http.proxy http://$userRede@proxylatam.indra.es:8080
+git config --global https.proxy https://$userRede@proxylatam.indra.es:8080
+
+#Apresenta as configurações
 git config -l
 
-Read-Host -Prompt 'Digite ENTER para sair'
+#Espera entrada de teclado para encerrar:
+$sair = Read-Host -Prompt 'Digite ENTER para sair'
